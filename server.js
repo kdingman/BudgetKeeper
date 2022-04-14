a3a5953affffbbbc20cd2f9de37e0f2075e3ce2e
kdingman <kcdonlan@yahoo.com>
2022-04-14 18:44:43 -0400

    heroku update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/BudgetKeeper";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/BudgetKeeper";
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
